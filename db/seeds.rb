4df81803aee365e807ee7ad0f3841c87ca29d2b2
James Berry <84353254+JamesRynnBerry@users.noreply.github.com>
2021-08-31 09:40:01 -0400

    completed Assignment

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Bird.create!(name: "Skwak", species: "Quack")
+Bird.create!(name: "Brrr", species: "None")
+Bird.create!(name: "Yarg", species: "Ton")
+Bird.create!(name: "Mfrg", species: "Bun")
